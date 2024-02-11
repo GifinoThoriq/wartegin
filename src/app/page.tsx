acabc1dbda05cd9973ecc5078085bff757ec07f3
Laksamana Zaidan Alfayyadh <laksamanazaidanalfayyad@192.168.0.111>
2024-02-11 17:00:49 +0700

    shadcn UI installation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
-      <h1>Wartegin</h1>
+      <div className="text-center mt-36">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          WartegIn
+        </h1>
+        <Button className="mt-5">Ceritanya Button</Button>
+      </div>
     </main>
   );
 }
